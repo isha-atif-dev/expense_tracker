d8c27876ab227189d949a989da68cd7f18b6047d
isha-atif-dev <isha.atif02@gmail.com>
2025-12-20 20:35:44 +0000

    New file added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # my_file
 This is my first github file
-isha atif
+<br>
+Isha atif
---
+++ main.py
@@ -0,0 +1,8 @@
+temp = 30
+if temp > 30:
+    print("true")
+elif temp == 30:
+    print("equal")
+else:
+    print("false")
+print("done")
